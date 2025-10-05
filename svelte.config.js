6924156faa345290dce00ac4a934ebbb0cc999e7
Triston Code <triston.personal01@gmail.com>
2025-10-05 11:46:02 -0500

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from "@sveltejs/adapter-cloudflare";
-
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,13 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      // Optional configuration
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
-    }),
+    adapter: adapter(),
   },
 };
 
