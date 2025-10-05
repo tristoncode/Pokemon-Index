a3ee01f3390afcda68797b5996279b1c82ee5105
Triston Code <triston.personal01@gmail.com>
2025-10-05 16:45:21 -0500

    updated ui

--- src/lib/components/Gallery/PokemonGallery.svelte
+++ src/lib/components/Gallery/PokemonGallery.svelte
@@ -5,16 +5,10 @@
   import pkStore from "$lib/stores/pkStore.svelte";
 
   $effect(() => {
-    let yellow = "oklch(79.5% 0.184 86.047)";
-    let defaultColor = "black";
-
-    const btns = document.querySelectorAll(".gallerySectionNavButton");
-    btns.forEach((btn) => {
-      if (btn.ariaLabel === pkStore.gallerySection) {
-        (btn as any).style.backgroundColor = yellow;
-      } else {
-        (btn as any).style.backgroundColor = defaultColor;
-      }
+    const galleryButtons = document.querySelectorAll("[data-gallerySectionNavButton]");
+    galleryButtons.forEach((btn) => {
+      if (btn.ariaLabel === pkStore.gallerySection) btn.classList.add("selected");
+      else btn.classList.remove("selected");
     });
   });
 </script>
@@ -26,19 +20,19 @@
   <h2 class="text-3xl font-bold" style="padding: 4px;">Gallery</h2>
 
   <nav>
-    <button aria-label="mainGallery" class="gallerySectionNavButton" onclick={() => (pkStore.gallerySection = "mainGallery")}>
+    <button aria-label="mainGallery" data-gallerySectionNavButton onclick={() => (pkStore.gallerySection = "mainGallery")}>
       <i class="bxr bx-images"></i>
     </button>
     <button
       aria-label="searchbarGallery"
-      class="gallerySectionNavButton"
+      data-gallerySectionNavButton
       onclick={() => (pkStore.gallerySection = "searchbarGallery")}
     >
       <i class="bxr bx-search-big"></i>
     </button>
     <button
       aria-label="alphabetGallery"
-      class="gallerySectionNavButton"
+      data-gallerySectionNavButton
       onclick={() => (pkStore.gallerySection = "alphabetGallery")}
     >
       A-Z
@@ -56,30 +50,26 @@
 
 <style lang="scss">
   nav {
-    min-height: 25px;
-    max-height: 32px;
-    margin: 8px auto;
     display: flex;
     gap: 4px;
+    margin: 8px auto;
 
     & > button {
-      min-width: 45px;
-      background-color: black;
-      color: #fafaf9;
+      width: 53px;
       padding: 4px;
+      background-color: black;
+      color: whitesmoke;
       border-radius: 2px;
       display: flex;
       align-items: center;
       justify-content: center;
-      transition: all 0.1s ease;
 
       &:hover {
-        transform: scale(1.1);
-        background-color: grey !important;
+        background-color: grey;
       }
 
       i {
-        color: #fafaf9;
+        color: whitesmoke;
       }
     }
   }
