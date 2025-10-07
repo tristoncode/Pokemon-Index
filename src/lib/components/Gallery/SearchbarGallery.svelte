06ff762788680acb68b7d3987bf187c46e7be3ac
Triston Code <triston.personal01@gmail.com>
2025-10-06 20:09:27 -0500

    updated ui and ux

--- src/lib/components/Gallery/SearchbarGallery.svelte
+++ src/lib/components/Gallery/SearchbarGallery.svelte
@@ -14,19 +14,24 @@
 
   async function oninput() {
     is_searched = true;
-    pokemonListObjs = await pkStore.group_searchbarGallery.handle_getSearchedPokemonResults(inputBox.value);
+    pokemonListObjs = await pkStore.group_searchbarGallery.handle_getSearchedPokemonResults(inputBox.value.toLowerCase());
   }
 </script>
 
-<div class="h-[100%] flex flex-col gap-2" style="margin-bottom: 4px;">
-  <input
-    type="text"
-    placeholder="Search for Pokemon..."
-    class="flex-1 bg-blue-100 border-2 border-blue-700 rounded-[2px] font-semibold"
-    style="padding: 4px;"
-    bind:this={inputBox}
-    {oninput}
-  />
+<div class="h-[100%] flex flex-col gap-[2px]">
+  <div>
+    <p class="max-w-fit bg-black text-left rounded-[2px_2px_0px_0px]" style="padding:0px 4px; color: white;">
+      Search for Pokemon
+    </p>
+    <input
+      type="text"
+      placeholder="Example: Pikachu"
+      class="w-[100%] bg-blue-100 border-2 border-black rounded-[0px_2px_2px_2px] font-semibold"
+      style="padding: 0 4px 4px 4px;"
+      bind:this={inputBox}
+      {oninput}
+    />
+  </div>
 
   <PokemonCont>
     {#if is_searched}
